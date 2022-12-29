f77228ff396dda0273497419eb341c903c4cb3af
Benson Mugwe <99112362+GasComIT@users.noreply.github.com>
2022-12-29 16:23:46 +0300

    🍑 ᴀʟɪᴄᴇ 💃🏽 ʍʊֆɨƈ 🍃

---
+++ 🍑ᴀʟɪᴄᴇ💃🏽/ᴀʟɪᴄᴇɦʊɮ/ꜱʜᴜꜰꜰʟᴇ_𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓.js
@@ -0,0 +1,145 @@
+// 🍑ᴀʟɪᴄᴇ Mʊֆɨƈ💃🏽by 🍃 GasComIT
+// https://github.com/GasComIT/Alice
+// https://i.postimg.cc/sXLzq8k1/Alice.jpg
+
+const { canModifyQueue, ᴀʟɪᴄᴇʄɨӼ, ᴀʟɪᴄᴇքʊʀɢɛʀ } = require("../ᴀʟɪᴄᴇռɛʋ/ʟᴏᴀᴅᴇʀ");
+const { MessageEmbed } = require("../𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓/src");
+module.exports = {
+  name: "shuffle",
+  cooldown: 3,
+  execute(message) {
+    try {
+      if (
+        message.content.startsWith(ᴀʟɪᴄᴇʄɨӼ + "shuffle") &&
+        message.channel.name !== "ᴀʟɪᴄᴇ💖ᴍᴜꜱɪᴄ"
+      ) {
+        message.react("❌");
+        message.react("🔥");
+        message.channel
+          .send(
+            new MessageEmbed()
+              .setColor("#FF5F15")
+              .setTitle("🍑ᴀʟɪᴄᴇ💃🏽by 🍃 GasComIT")
+              .setAuthor(`🥴 Warning! ⚠️`)
+              .setURL("https://github.com/GasComIT?tab=repositories")
+              .setThumbnail(`https://i.postimg.cc/sXLzq8k1/Alice.jpg`)
+              .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: ɢɴᴜ(ᴄ)2023 𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓")
+              .setDescription(`**Ú§êr >** ${message.author}
+
+**Category:** \`🔱Gascomit\`
+**Channel:** \`ᴀʟɪᴄᴇ❣️ʍʊֆɨƈ\`
+⚠️Please use 👆🏻 for any **ᴀʟɪᴄᴇ** commands.`)
+          )
+          .catch(console.error)
+          .then((message) => {
+            message.delete({
+              timeout: `${ᴀʟɪᴄᴇքʊʀɢɛʀ}`,
+            });
+          });
+        return;
+      }
+      if (
+        message.content.startsWith(ᴀʟɪᴄᴇʄɨӼ + "shuffle") &&
+        message.channel.name === "ᴀʟɪᴄᴇ💖ᴍᴜꜱɪᴄ"
+      ) {
+        const queue = message.client.queue.get(message.guild.id);
+        if (!queue) {
+          message.react("❌");
+          message.react("🔥");
+          message.channel
+            .send(
+              new MessageEmbed()
+                .setColor("#FF5F15")
+                .setTitle("🍑ᴀʟɪᴄᴇ💃🏽by 🍃 GasComIT")
+                .setAuthor(`🥴 Warning! ⚠️`)
+                .setURL("https://github.com/GasComIT?tab=repositories")
+                .setThumbnail(`https://i.postimg.cc/sXLzq8k1/Alice.jpg`)
+                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: ɢɴᴜ(ᴄ)2023 𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓")
+                .setDescription(`**Ú§êr >** ${message.author}
+
+There is nothing in queue.`)
+            )
+            .catch(console.error)
+            .then((message) => {
+              message.delete({
+                timeout: `${ᴀʟɪᴄᴇքʊʀɢɛʀ}`,
+              });
+            });
+          return;
+        }
+        if (!canModifyQueue(message.member)) {
+          message.react("❌");
+          message.react("🔥");
+          message.channel
+            .send(
+              new MessageEmbed()
+                .setColor("#FF5F15")
+                .setTitle("🍑ᴀʟɪᴄᴇ💃🏽by 🍃 GasComIT")
+                .setAuthor(`🥴 Warning! ⚠️`)
+                .setURL("https://github.com/GasComIT?tab=repositories")
+                .setThumbnail(`https://i.postimg.cc/sXLzq8k1/Alice.jpg`)
+                .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: ɢɴᴜ(ᴄ)2023 𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓")
+                .setDescription(`**Ú§êr >** ${message.author}
+
+*You need to* **join** *a voice channel first!*`)
+            )
+            .catch(console.error)
+            .then((message) => {
+              message.delete({
+                timeout: `${ᴀʟɪᴄᴇքʊʀɢɛʀ}`,
+              });
+            });
+          return;
+        }
+        let songs = queue.songs;
+        for (let i = songs.length - 1; i > 1; i--) {
+          let j = 1 + Math.floor(Math.random() * i);
+          [songs[i], songs[j]] = [songs[j], songs[i]];
+        }
+        queue.songs = songs;
+        message.client.queue.set(message.guild.id, queue);
+        message.react("💃🏽");
+        message.react("🍑");
+        queue.textChannel
+          .send(
+            new MessageEmbed()
+              .setColor("#ff0000")
+              .setAuthor(`🥴 Warning! ⚠️`)
+              .setTitle("🍑ᴀʟɪᴄᴇ💃🏽by 🍃 GasComIT")
+              .setURL("https://github.com/GasComIT?tab=repositories")
+              .setThumbnail(`https://i.postimg.cc/sXLzq8k1/Alice.jpg`)
+              .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: ɢɴᴜ(ᴄ)2023 𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓")
+              .setDescription(`**Ú§êr >** ${message.author}
+
+🔀 Shuffled the queue`)
+          )
+          .catch(console.error)
+          .then((message) => {
+            message.delete({
+              timeout: `${ᴀʟɪᴄᴇքʊʀɢɛʀ}`,
+            });
+          });
+      }
+    } catch (ErrorAlice) {
+      message.channel.send(
+        new MessageEmbed()
+          .setColor("#DB4434")
+          .setAuthor("🔺ERROR CAUGHT🔻")
+          .setTitle("🍑ᴀʟɪᴄᴇ💃🏽by 🍃 GasComIT")
+          .setFooter("🔰𝗟𝗶𝗰𝗲𝗻𝘀𝗲: ɢɴᴜ(ᴄ)2023 𝐆𝐚𝐬𝐂𝐨𝐦𝐈𝐓")
+          .setThumbnail("https://i.postimg.cc/sXLzq8k1/Alice.jpg")
+          .setDescription(`**ᴀʟɪᴄᴇ❣️ʍʊֆɨƈ** has encountered an error.
+
+Please report to either 
+🔰**In Discord channel** = https://discord.gg/xx
+or
+🔰**In telegram group** = https://t.me/Gascomit
+
+
+**🔺Error Caught🔻**
+*${ErrorAlice}*`)
+      );
+      console.error(ErrorAlice);
+    }
+  },
+};
